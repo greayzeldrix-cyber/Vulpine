a43ae143f9299719e8c5709085eede3ed59684c1
greayzeldrix-cyber <greay.zeldrix@gmail.com>
2026-02-14 22:47:04 +0100

    Init Commit

---
+++ src/main/java/net/greayzeldrix/vulpine/Vulpine.java
@@ -0,0 +1,10 @@
+package net.greayzeldrix.vulpine;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Vulpine implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/net/greayzeldrix/vulpine/client/VulpineClient.java
@@ -0,0 +1,10 @@
+package net.greayzeldrix.vulpine.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VulpineClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
